ab1a4aadd279156a8036c31671a91653a752b7aa
KreedoEducation <79310045+KreedoEducation@users.noreply.github.com>
2021-06-15 10:41:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Reading-Numbers-11-19
+ Learn LetterTracing, Identify correct numbers and more . This game is developed by Sudeep for KEPL
---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+
+{
+	const scriptsInEvents = {
+
+		async Es_common_Event310_Act1(runtime, localVars)
+		{
+			end(runtime);
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
