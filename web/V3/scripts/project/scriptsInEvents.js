bfeffa8aa13c9d1241c4ace2971511aaa0cb2c40
Kartik Singh <kartik.singh4f@gmail.com>
2021-08-23 14:27:10 +0530

    rework build 1

---
+++ web/V3/scripts/project/scriptsInEvents.js
@@ -0,0 +1,98 @@
+"use strict";
+
+export class AppInterface  {
+
+	static showToast(msg){
+		let jsonToast = {};
+			jsonToast.toast = msg;
+        console.log('showToast', jsonToast);
+		   if (window && window.ReactNativeWebView) {
+		   
+			window.ReactNativeWebView.postMessage(jsonToast);
+		}
+		
+	}
+	static sendToApp(data){		
+		let json_str = JSON.stringify(data);
+		console.log("Send data to Webview", json_str);
+		if (window && window.ReactNativeWebView) {
+			window.ReactNativeWebView.postMessage(json_str);
+		}
+		
+
+	}
+}
+
+{
+	const scriptsInEvents = {
+
+		async Egame_Event1_Act2(runtime, localVars)
+		{
+			var currentdate = new Date(); 
+			var datetime = currentdate.getDate() + "/"
+			                + (currentdate.getMonth()+1)  + "/" 
+			                + currentdate.getFullYear() + "@"  
+			                + currentdate.getHours() + ":"  
+			                + currentdate.getMinutes() + ":" 
+			                + currentdate.getSeconds();
+							
+			runtime.globalVars.StartDateTime = datetime;
+		},
+
+		async Egame_Event1_Act4(runtime, localVars)
+		{
+			const nextLevel = runtime.globalVars.GameLevel;
+				if(nextLevel == 0){
+				runtime.goToLayout("Level"+nextLevel+"-Start");
+				}else if(eval("runtime.globalVars.L"+nextLevel+"_Tutorial_Done")){
+				runtime.goToLayout("Level"+nextLevel);
+				}
+			else{
+			runtime.goToLayout("Level"+nextLevel+"-Tutorial");
+			}
+			
+		},
+
+		async Es_common_Event47_Act1(runtime, localVars)
+		{
+			
+			const nextLevel = runtime.globalVars.CurrentLevel+1;
+				if(eval("runtime.globalVars.L"+nextLevel+"_Tutorial_Done")){
+				runtime.goToLayout("Level"+nextLevel);
+				}
+			else{
+			runtime.goToLayout("Level"+nextLevel+"-Tutorial");
+			}
+			
+		},
+
+		async Es_common_Event48_Act1(runtime, localVars)
+		{
+			
+			const previousLevel = runtime.globalVars.CurrentLevel-1;
+			
+			if(previousLevel==0){
+					runtime.goToLayout("Level"+previousLevel+"-Start");
+			}
+				else if(eval("runtime.globalVars.L"+previousLevel+"_Tutorial_Done")){
+				runtime.goToLayout("Level"+previousLevel);
+				}
+			else{
+			runtime.goToLayout("Level"+previousLevel+"-Tutorial");
+			}
+			
+		},
+
+		async Es_common_Event184_Act48(runtime, localVars)
+		{
+			localStorage.setItem(runtime.globalVars.LOCAL_GAME_KEY, JSON.stringify(runtime.getInstanceByUid(5617).getJsonDataCopy()));
+			if(AppInterface!="undefined")
+			AppInterface.sendToApp(runtime.getInstanceByUid(5617).getJsonDataCopy());
+			else console.log("AppInterface is not defined");
+			
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
